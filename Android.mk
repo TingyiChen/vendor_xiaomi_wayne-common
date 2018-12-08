aed499207dc552bf333f0886df75564692fa468d
Isaac Chen <isaacchen@isaacchen.cn>
2018-12-08 16:06:54 +0000

    wayne-common: Apply jasmine rebrand
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter jasmine wayne,$(TARGET_DEVICE)),)
+ifneq ($(filter jasmine_sprout wayne,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libantradio
